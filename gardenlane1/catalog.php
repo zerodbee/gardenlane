6439ff35570339b2f1a7dda552e329b43dd0c7f3
zerodbee <125971837+zerodbee@users.noreply.github.com>
2023-06-07 12:51:44 +0300

    first commit

---
+++ gardenlane1/catalog.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet'  href="./styles/catalog.css">
+    <title>Каталог</title>
+</head>
+<body>
+<?php
+
+    include "details/header1.php";
+    include "details/header.php";
+
+    ?>
+
+        <div class="menu-top">
+            <div class="hrefs">
+                <a class="aa" href="index.php">Главная страница / </a><a class="bb"  href="catalog.php">Каталог</a>
+            </div>
+            <div>
+            <form class="poisk" method="POST">
+            <input class="place" class="" type="text" placeholder="Поиск">
+            <input class="button1" type="button">
+            </form>
+            </div>
+        </div>
+        <hr class="line1" color="#dadada" size="1px" style="margin-right: 100px;     margin-left: 100px; margin-top: 15px;">
+
+
+
+
+
+
+
+
+        <div class="katalog1">
+
+            <div class="catt1">
+                    <ul class="ppp3">
+                            <p class="nazvanie1">Подарки</p>
+                            <li><a class="lab" href="">Подарочные наборы</a></li>
+                            <li><a class="lab" href="">Конфеты и торты</a></li>
+                            <li><a class="lab" href="">Подарочные корзины</a></li>
+                            <li><a class="lab" href="">Дополнения к букетам</a></li>
+                    </ul>
+                    <img class="imgimg" src="./png/a.png" alt="" height="180px">
+            </div>
+
+            <div class="catt2">
+                    <ul class="ppp3">
+                            <p class="nazvanie1">Цветы</p>
+                            <li><a class="lab1" href="">Бизнес-букеты</a></li>
+                            <li><a  class="lab1" href="">Необычные</a></li>
+                            <li><a  class="lab1" href="">Крафт</a></li>
+                            <li><a  class="lab1" href="">VIP-букеты</a></li>
+                            <li><a  class="lab1" href="">Полевые цветы</a></li>
+                    </ul>
+                    <img class="imgimg5" src="./png/b.png" alt="" height="180px">
+            </div>
+
+            <div class="catt3">
+                    <ul class="ppp3">
+                            <p class="nazvanie1">Композиции</p>
+                            <li><a  class="lab2" href="">Цветы в корзинах</a></li>
+                            <li><a  class="lab2" href="">Цветы в коробках</a></li>
+                            <li><a  class="lab2" href="">Подароные корзины</a></li>
+                            <li><a  class="lab2" href="">Продуктовые корзины</a></li>
+                    </ul>
+                    <img class="imgimg" src="./png/c.png" alt="" height="180px">
+            </div>
+
+        </div>
+
+        <div class="katalog2">
+
+                <div class="catt4">
+                    <ul class="ppp3">
+                        <p class="nazvanie1">Съедобные букеты</p>
+                        <li><a  class="lab3" href="">Клубника в шоколаде</a></li>
+                        <li><a  class="lab3" href="">Продуктовые букеты</a></li>
+                        </ul>
+                        <img class="imgimg1" src="./png/d.png" alt="" height="100px">
+            </div>
+
+                <div class="catt5">
+                    <ul class="ppp3">
+                        <p class="nazvanie1">Воздушные шары</p>
+                        <li><a  class="lab4" href="">Букеты из шаров</a></li>
+                        <li><a  class="lab4" href="">Шары с наполнениями</a></li>
+                        <li><a  class="lab4" href="">Все шары</a></li>
+                        </ul>
+                        <img class="imgimg11" src="./png/e.png" alt="" height="100px">
+            </div>
+                
+                <div class="catt6">
+                    <ul class="ppp3">
+                        <p class="nazvanie1">Открытки и игрушки</p>
+                        <li><a  class="lab5" href="">Открытки ручной работы</a></li>
+                        <li><a  class="lab5" href="">Игрушки</a></li>
+                        </ul>
+                        <img class="imgimg111" src="./png/f.png" alt="" height="130px">
+            </div>
+
+        </div>
+        
+        <hr class="line1" color="#dadada" size="1px" style="margin-right: 100px;     margin-left: 100px; margin-top: 50px;">
+
+
+</body>
+</html>
---
+++ gardenlane1/details/header.php
@@ -0,0 +1,44 @@
+<link rel='stylesheet'  href="./styles/style.css">
+
+<div class="header">
+
+    <div class="lineone">
+
+        <a class="logo" href="./index.php">Garden Lane<!--<img class="logo" src="./png/logo.png" alt="">--></a>
+        <a class="phone" href="tel:+99999999999"><p >+9-(999)-999-99-99</p></a>
+    </div>
+
+        <hr class="line" color="#dadada" size="1px">
+
+    <div class="menu">
+
+    <nav class="catalogmenu">
+		<ul class="catalog"> 
+            <li ><a class="cat" href="">Акции и Скидки</a></li>
+            <li ><a class="cat" href="./catalog.php">Каталог</a>
+                <ul class ="catalog2">
+                    <li><a class="cat2" href="">Подарки</a></li>
+                    <hr class="line1" color="#dadada" size="1px">
+                    <li><a class="cat2" href="">Цветы и букеты</a></li>
+                    <hr class="line1" color="#dadada" size="1px">
+                    <li><a class="cat2" href="">Композиции</a></li>
+                    <hr class="line1" color="#dadada" size="1px">
+                    <li><a class="cat2" href="">Съедобные букеты</a></li>
+                    <hr class="line1" color="#dadada" size="1px">
+                    <li><a class="cat2" href="">Воздушные шары</a></li>
+                    <hr class="line1" color="#dadada" size="1px">
+                    <li><a class="cat2" href="">Открытки</a></li>
+                </ul>
+            </li>
+			<li ><a class="cat" href="#">Подарки</a></li>
+        </ul>
+    </nav>
+
+    </div>
+
+</div>    
+
+
+ 
+
+
